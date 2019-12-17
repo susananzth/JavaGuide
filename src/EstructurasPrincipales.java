201da32c8cb5b936a51837970c21e330667e7bdd
Admin-WIN <susananzth@gmail.com>
2019-12-17 12:59:41 -0500

    Agregando clase de estructura pincipal de java

---
+++ src/EstructurasPrincipales.java
@@ -0,0 +1,40 @@
+
+public class EstructurasPrincipales {
+
+	public static void main(String args[]) {
+		System.out.println("Hola Mundo");
+		/* Una impresión en consola de la oración "Hola mundo" */
+		
+		/*===== TIPOS DE DATOS =====*/
+		
+		/* Números enteros:
+		 *    Int: 4 bytes de almacenamiento. Desde -2.147.483.648 hasta 2.147.483.647
+		 * 	  Short: 2 byte de almacenamiento. Desde -32.768 hasta 32.767
+		 *    Long: 8 bytes de almacenamiento. Desde Una barbaridad. Sufijo L.
+		 *    Byte: 1 byte de almacenamiento. Desde -128 hasta 127
+		 * 
+		 * Coma flotante (Decimales):
+		 *    Float: 4 bytes de almacenamiento. Aprox 6-7 cifras de decimales. Sufijo F. Ej: 3.25F
+		 *    Double: 8 bytes de almacenamiento. Aprox 15 cifras decimales. No lleva sufijo.
+		 *    
+		 * Caracteres:
+		 *    Char: para representar caracteres. 
+		 *    
+		 * Booleano:
+		 *    Boolean: puede ser true o false.
+		 * */
+		
+		/*===== USO DE VARIABLE =====*/
+		
+		/* Se declara la variables para indicar al ordenador que guarde un espacio 
+		 * en la memoria RAM. Declaramos una variable de la siguiente forma: */
+			int salario;
+			
+		/* Al inicializar la variable, estamos indicando que en el espacio almacenado 
+		 * en la RAM, irá el valor indicado. Inicializamos una variable de la siguiente forma: */
+			salario = 5000;
+		/* 
+		 * */
+	}
+	
+}
