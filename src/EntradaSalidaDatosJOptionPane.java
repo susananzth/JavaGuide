f79b99dde5b6001e07804aa8310fafef8bd867de
Admin-WIN <susananzth@gmail.com>
2019-12-18 17:15:25 -0500

    Avance en JOptionPane.  Ejemplos.

--- src/EntradaSalidaDatosJOptionPane.java
+++ src/EntradaSalidaDatosJOptionPane.java
@@ -14,8 +14,14 @@ public class EntradaSalidaDatosJOptionPane {
 		
 		System.out.println("Hola " + nombreUsuario + ", tienes " + (edad) + " años.");
 		
+		
+		//Ejemplo donde uno de los input necesitamos que sea 'int' para hacer una operación numérica.
 		int edadUsuario = Integer.parseInt(edad); //Cambiamos a entero el string.
 		System.out.println("Hola " + nombreUsuario + ", el año que viene tendrás " + (edadUsuario+1) + " años.");
+		
+		//Ejemplo donde uno de los input necesitamos que sea 'double' para hacer una operación numérica.
+		String num1 = JOptionPane.showInputDialog("Intrduce un número");
+		double num2 = Double.parseDouble(num1);
 
 	}
 
